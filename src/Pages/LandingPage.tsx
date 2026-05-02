0e1b95c7e660371054e6dacc48e44a29ee3d480f
Maki Maki <makiaboabidamaki@gmail.com>
2026-05-02 01:08:53 -0400

    Create the technology section under hero section

--- src/Pages/LandingPage.tsx
+++ src/Pages/LandingPage.tsx
@@ -1,11 +1,13 @@
 import Hero from "../Components/Hero/Hero";
 import Navbar from "../Components/Navbar/Navbar";
+import Technologies from "../Components/Technologies/Technolgies";
 
 const LandingPage: React.FC = () => {
   return (
     <div>
         <Navbar />
         <Hero />
+        <Technologies />
     </div>
   );
 };
