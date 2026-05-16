498284f70fe4c15064f614607ddb66bac11fe0d0
Maki Maki <makiaboabidamaki@gmail.com>
2026-05-16 17:37:11 -0400

    remove unused var

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import type { FC } from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import LandingPage from "./Pages/LandingPage";
 import Projects from "./Pages/Projects";
